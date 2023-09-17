b53df1adfe18a57c50c46bdcf623b03948ef63ed
Digicat509 <alexander.snyder@tagmagnet.org>
2023-09-17 14:50:57 -0500

    Added images to smart/dumb drone enemys

--- Games/catformer/DumbDroneEnemy.java
+++ Games/catformer/DumbDroneEnemy.java
@@ -2,6 +2,9 @@ package catformer;
 
 import java.awt.Color;
 import java.awt.Graphics;
+import java.io.IOException;
+
+import javax.imageio.ImageIO;
 
 import gameEngine.GameObject;
 
@@ -12,12 +15,16 @@ public class DumbDroneEnemy extends Enemy {
 	
 	public DumbDroneEnemy(int x, int y) {
 		super(x, y, 0);
-		w = 15;
-		h = 15;
+		w = 22;
+		h = 12;
+		try {
+			this.image = ImageIO.read(getClass().getResource("assets/RollyPolly.png"));
+		} catch (Exception e) {e.printStackTrace();}
 	}
 	public void draw(Graphics g) {
-		g.setColor(Color.MAGENTA);
-		g.fillRect((int)x, (int)y, w, h);
+		//g.setColor(Color.MAGENTA);
+		//g.fillRect((int)x, (int)y, w, h);
+		g.drawImage(image, (int)x, (int)y, null);
 		move();
 	}
 	@Override
@@ -30,6 +37,11 @@ public class DumbDroneEnemy extends Enemy {
 			onGround = true;
 			if(Platformer.player.x < this.x && Platformer.player.x > this.x-RANGE) {
 				dx = -4;
+				try {
+					this.image = ImageIO.read(getClass().getResource("assets/RollingPolly.png"));
+					w = 15;
+					h = 15;
+				} catch (Exception e) {e.printStackTrace();}
 			}
 			dy = 0;
 			if(o.y>= this.y) {
--- Games/catformer/SmartDroneEnemy.java
+++ Games/catformer/SmartDroneEnemy.java
@@ -3,21 +3,31 @@ package catformer;
 import java.awt.Color;
 import java.awt.Graphics;
 
+import javax.imageio.ImageIO;
+
 import gameEngine.GameObject;
 
 public class SmartDroneEnemy extends Enemy {
 	
 	private float gravity = 0.5f;
 	private static final int RANGE = 100;
+	private boolean facing = true;
 	
 	public SmartDroneEnemy(int x, int y) {
 		super(x, y, 3);
-		w = 15;
-		h = 15;
+		w = 22;
+		h = 12;
+		try {
+			this.image = ImageIO.read(getClass().getResource("assets/Beetle.png"));
+		} catch (Exception e) {e.printStackTrace();}
 	}
 	public void draw(Graphics g) {
-		g.setColor(Color.pink);
-		g.fillRect((int)x, (int)y, w, h);
+		//g.setColor(Color.pink);
+		//g.fillRect((int)x, (int)y, w, h);
+		if(facing)
+			g.drawImage(image,(int)x, (int)y, w, h, null);
+		else 
+			g.drawImage(image,(int)x+w, (int)y, -w, h, null);
 		move();
 	}
 	@Override
@@ -29,9 +39,11 @@ public class SmartDroneEnemy extends Enemy {
 		{
 			if(Platformer.player.x < this.x && Platformer.player.x > this.x-RANGE) {
 				x -= dx;
+				facing = true;
 			}
 			if(Platformer.player.x > this.x && Platformer.player.x < this.x+this.w+RANGE) {
 				x += dx;
+				facing = false;
 			}
 			if(x < o.x) {
 				x = o.x;
