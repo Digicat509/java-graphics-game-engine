48bd840a0bce41f6d49e81eb61841f7f43943282
Cris Ovalle <cris.ovalle@tagmagnet.org>
2023-10-09 15:14:21 -0500

    clean up

--- Games/catformer/Player.java
+++ Games/catformer/Player.java
@@ -95,19 +95,13 @@ public class Player extends GameObject{
 				dx += speed;
 			else
 			{
-				//x += speed;
-				//localX += speed;
 				dx += speed;
 				
 				GameObject o = this.hits();
 				if(o != null)
 				{
 					dx -= speed;
-					//x -= speed;
 					sideTouch = true;
-					//testJumps(o);
-					//System.out.println("touch");
-					//localX -= speed;
 				}
 			}
 			
@@ -122,16 +116,11 @@ public class Player extends GameObject{
 			else if(x > 0)
 			{
 				dx -= speed;
-				//x -= speed;
-				//localX -= speed;
+
 				GameObject o = this.hits();
 				if(o != null)
 				{
 					dx += speed;
-					//x += speed;
-					//sideTouch = true;
-					//testJumps(o);
-					//localX += speed;
 				}
 			}
 		}
@@ -160,59 +149,11 @@ public class Player extends GameObject{
 		}
 	}
 	
-	public void testJumps(GameObject o) {
-		Platformer.game.getHandeler().forEach(other -> {if(!other.equals(this))other.x -= this.dx;});
-
-		if(sideTouch) {
-			sliding = true;
-			System.out.println("asvdiufa sdfiuwheiufd dsfda");
-			
-			if(onGround==false) {
-				System.out.println("On Ground");
-			}
-			if((Platformer.game.getInput().isKey(KeyEvent.VK_W) || Platformer.game.getInput().isKey(KeyEvent.VK_UP))) {
-//				System.out.println("Wall Jump");
-//				onGround=true;
-//				dy=-slidingGravity;
-				System.out.println("WallJump?");
-				wallJump = false;
-				dy = -jumpStrength;
-				if(Platformer.game.getInput().isKey(KeyEvent.VK_A))
-					dx=-1;
-				if(Platformer.game.getInput().isKey(KeyEvent.VK_D))
-					dx=1;
-				dx *= 10;
-				sliding = false;
-				waitTime = System.currentTimeMillis()+200;
-			}
-
-			
-			if(onGround == false && wallJump && (Platformer.game.getInput().isKey(KeyEvent.VK_W) || Platformer.game.getInput().isKey(KeyEvent.VK_UP))) {
-				System.out.println("working!!!");
-				wallJump = false;
-				dy = -jumpStrength;
-				dx *= 10;
-				sliding = false;
-				waitTime = System.currentTimeMillis()+200;
-			}
-		}
-		//o = this.hits();
-
-		else {
-			if(waitTime <= System.currentTimeMillis())
-				wallJump = true;
-			sliding = false;
-		}
-
-		
-	}
-	
 	public void collisionJumps(GameObject o) {
 		//Platformer.game.getHandeler().forEach(other -> {if(!other.equals(this))other.x -= this.dx;});
 		updatePosition(1);
 		
 		o = this.hits();
-		//System.out.println(this.allHits()+"\n\n\n\n");
 
 		if(o instanceof Platform)
 		{
@@ -229,7 +170,7 @@ public class Player extends GameObject{
 					dy = -jumpStrength;
 					dx *= 10;
 					sliding = false;
-					waitTime = System.currentTimeMillis()+150;
+					waitTime = System.currentTimeMillis()+250;
 				}
 			}
 			localX -= dx;
@@ -294,10 +235,8 @@ public class Player extends GameObject{
 				dy = 0;
 				y = o.y-h;
 				onGround = true;
-				//System.out.println("over");
 			}
 			if(y >= o.y+o.h-5) {
-				//System.out.println("under");
 				dy = 0;
 				y = o.y+o.h;
 			}
@@ -311,9 +250,7 @@ public class Player extends GameObject{
 		}
 		localX += dx;
 		
-		//System.out.println("Collision jumps?");
 		collisionJumps(o);
-		//testJumps(o);
 		
 		if(x < 0)
 			x = 0;
